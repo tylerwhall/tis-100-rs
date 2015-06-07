599d667ac89acd0623c6d6e5f0f8161b4e3a332c
Tyler Hall <tylerwhall@gmail.com>
2015-06-07 18:03:35 -0400

    Tests for instruction conversion

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Instruction {
     NOP,
     MOV { src: Operand, dst: Operand },
@@ -12,6 +12,9 @@ pub enum Instruction {
     J { cond: Condition, dst: Label},
     JRO { dst: Operand },
 }
+
+static BAD_OPCODE_ERR: &'static str = "Bad opcode for # of arguments";
+static NUM_ARGS_ERR: &'static str = "Wrong number of arguments";
 
 impl FromStr for Instruction {
     type Err = &'static str;
@@ -25,7 +28,7 @@ impl FromStr for Instruction {
                 "SWP" => Ok(Instruction::SWP),
                 "SAV" => Ok(Instruction::SAV),
                 "NEG" => Ok(Instruction::NEG),
-                _ => Err("Bad opcode for # of arguments"),
+                _ => Err(BAD_OPCODE_ERR),
             },
 
             2 => match words[0] {
@@ -37,21 +40,50 @@ impl FromStr for Instruction {
                 "JGZ" => Ok(Instruction::J { cond: Condition::Gz,            dst: words[1].to_string() }),
                 "JLZ" => Ok(Instruction::J { cond: Condition::Lz,            dst: words[1].to_string() }),
                 "JRO" => Operand::from_str(words[1]).map(|o| Instruction::JRO { dst: o }),
-                _ => Err("Bad opcode for # of arguments"),
+                _ => Err(BAD_OPCODE_ERR),
             },
 
             3 => match words[0] {
                 "MOV" => Operand::from_str(words[1]).and_then(|s| Operand::from_str(words[2]).map(|d| (s, d)))
                             .map(|(s, d)| Instruction::MOV { src: s, dst: d }),
-                _ => Err("Bad opcode for # of arguments"),
+                _ => Err(BAD_OPCODE_ERR),
             },
 
-            _ => Err("number of args")
+            _ => Err(NUM_ARGS_ERR)
         }
     }
 }
 
-#[derive(Debug)]
+#[test]
+fn instruction_from_str() {
+    fn i(s: &str) -> Instruction {
+        Instruction::from_str(s).unwrap()
+    }
+    assert_eq!(i("NOP"), Instruction::NOP);
+    assert_eq!(i("SWP"), Instruction::SWP);
+    assert_eq!(i("SAV"), Instruction::SAV);
+    assert_eq!(i("NEG"), Instruction::NEG);
+    assert_eq!(Instruction::from_str("BOGUS").unwrap_err(), BAD_OPCODE_ERR);
+
+    assert_eq!(i("ADD 10"), Instruction::ADD { addend: Operand::Lit(10) });
+    assert_eq!(i("SUB 10"), Instruction::SUB { subtrahend: Operand::Lit(10) });
+    assert_eq!(i("JMP LOC"), Instruction::J { cond: Condition::Unconditional,   dst: "LOC".to_string() });
+    assert_eq!(i("JEZ LOC"), Instruction::J { cond: Condition::Ez,              dst: "LOC".to_string() });
+    assert_eq!(i("JGZ LOC"), Instruction::J { cond: Condition::Gz,              dst: "LOC".to_string() });
+    assert_eq!(i("JLZ LOC"), Instruction::J { cond: Condition::Lz,              dst: "LOC".to_string() });
+    assert_eq!(i("JRO 1"), Instruction::JRO { dst: Operand::Lit(1) });
+    assert_eq!(Instruction::from_str("JEQ LOC").unwrap_err(), BAD_OPCODE_ERR);
+
+    assert_eq!(i("MOV UP DOWN"),    Instruction::MOV { src: Operand::Port(Port::Up), dst: Operand::Port(Port::Down) });
+    //assert_eq!(i("MOV  UP  DOWN"), Instruction::MOV { src: Operand::Port(Port::Up), dst: Operand::Port(Port::Down) });
+    assert_eq!(i("MOV UP ACC"),     Instruction::MOV { src: Operand::Port(Port::Up), dst: Operand::ACC });
+    assert_eq!(i("MOV ACC ACC"),    Instruction::MOV { src: Operand::ACC, dst: Operand::ACC });
+    assert_eq!(Instruction::from_str("MV UP ACC").unwrap_err(), BAD_OPCODE_ERR);
+
+    assert_eq!(Instruction::from_str("1 2 3 4").unwrap_err(), NUM_ARGS_ERR);
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Port {
     Up,
     Down,
@@ -73,7 +105,16 @@ impl FromStr for Port {
     }
 }
 
-#[derive(Debug)]
+#[test]
+fn port_from_str() {
+    assert_eq!(Port::from_str("UP").unwrap(),   Port::Up);
+    assert_eq!(Port::from_str("DOWN").unwrap(), Port::Down);
+    assert_eq!(Port::from_str("LEFT").unwrap(), Port::Left);
+    assert_eq!(Port::from_str("RIGHT").unwrap(), Port::Right);
+    assert_eq!(Port::from_str("OTHER").unwrap_err(), "bad port");
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Operand {
     Lit(i32),
     Port(Port),
@@ -102,7 +143,15 @@ impl FromStr for Operand {
     }
 }
 
-#[derive(Debug)]
+#[test]
+fn operand_from_str() {
+    assert_eq!(Operand::from_str("ACC").unwrap(), Operand::ACC);
+    assert_eq!(Operand::from_str("32").unwrap(), Operand::Lit(32));
+    assert_eq!(Operand::from_str("UP").unwrap(), Operand::Port(Port::Up));
+    assert_eq!(Operand::from_str("FOO").unwrap_err(), "Invalid operand");
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Condition {
     Unconditional,
     Ez,
