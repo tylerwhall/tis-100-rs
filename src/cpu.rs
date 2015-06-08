30ade629f09df483a21dd268721e5af0e91dc8cd
Tyler Hall <tylerwhall@gmail.com>
2015-06-08 10:40:16 -0400

    cpu: move state to separate struct
    This allows the state to be mutated while holding a borrow on the
    executable.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,18 +1,21 @@
 use parse::Executable;
 use instruction::{Instruction, Condition, Operand, Port};
 
-struct Cpu {
-    pc: i32,
+#[derive(Default)]
+pub struct CpuState {
     pub acc: i32,
     pub bak: i32,
-    executable: Executable
+    pc: i32,
+}
+
+pub struct Cpu {
+    pub state: CpuState,
+    executable: Executable,
 }
 
 impl Cpu {
     pub fn new(executable: Executable) -> Cpu {
-        Cpu { pc: 0,
-              acc: 0,
-              bak: 0,
+        Cpu { state: Default::default(),
               executable: executable,
         }
     }
@@ -28,47 +31,47 @@ impl Cpu {
                 match self.get_operand(src) {
                     Some(i) => match dst {
                             &Operand::Lit(_) => panic!("Cannot store to a literal"),
-                            &Operand::ACC => { self.acc = i; true },
+                            &Operand::ACC => { self.state.acc = i; true },
                             &Operand::Port(ref p) => self.write_port(p.to_owned(), i),
                     },
                     None => false
                 }
             },
             Instruction::SWP => {
-                let tmp = self.acc;
-                self.acc = self.bak;
-                self.bak = tmp;
+                let tmp = self.state.acc;
+                self.state.acc = self.state.bak;
+                self.state.bak = tmp;
                 true
             },
             Instruction::SAV => {
-                self.bak = self.acc;
+                self.state.bak = self.state.acc;
                 true
             },
             Instruction::ADD { ref addend } => {
                 match self.get_operand(addend) {
-                    Some(i) => { self.acc += i; true },
+                    Some(i) => { self.state.acc += i; true },
                     None => false
                 }
             },
             Instruction::SUB { ref subtrahend } => {
                 match self.get_operand(subtrahend) {
-                    Some(i) => { self.acc -= i; true },
+                    Some(i) => { self.state.acc -= i; true },
                     None => false
                 }
             },
             Instruction::NEG => {
-                self.acc = -self.acc;
+                self.state.acc = -self.state.acc;
                 true
             },
             Instruction::J { ref cond, ref dst } => {
                 if match *cond {
                     Condition::Unconditional => true,
-                    Condition::Ez => self.acc == 0,
-                    Condition::Nz => self.acc != 0,
-                    Condition::Gz => self.acc > 0,
-                    Condition::Lz => self.acc < 0,
+                    Condition::Ez => self.state.acc == 0,
+                    Condition::Nz => self.state.acc != 0,
+                    Condition::Gz => self.state.acc > 0,
+                    Condition::Lz => self.state.acc < 0,
                 } {
-                    self.pc = self.executable.label_line(dst) as i32;
+                    self.state.pc = self.executable.label_line(dst) as i32;
                     false
                 } else {
                     true
@@ -76,18 +79,18 @@ impl Cpu {
             },
             Instruction::JRO { ref dst } => {
                 match self.get_operand(dst) {
-                    Some(i) => { self.pc += i; true },
+                    Some(i) => { self.state.pc += i; true },
                     None => false
                 }
             },
         };
         if advance_pc {
-            self.pc += 1;
+            self.state.pc += 1;
         }
         /* Handle wrapping at the end and via JRO */
-        self.pc %= self.executable.len() as i32;
-        if self.pc < 0 {
-            self.pc = self.executable.len() as i32 + self.pc;
+        self.state.pc %= self.executable.len() as i32;
+        if self.state.pc < 0 {
+            self.state.pc = self.executable.len() as i32 + self.state.pc;
         }
         true
     }
@@ -107,13 +110,13 @@ impl Cpu {
     fn get_operand(&self, op: &Operand) -> Option<i32> {
         match op {
             &Operand::Lit(i) => Some(i),
-            &Operand::ACC => Some(self.acc),
+            &Operand::ACC => Some(self.state.acc),
             &Operand::Port(ref p) => self.read_port(p.to_owned()),
         }
     }
 
     fn pc(&self) -> usize {
-        self.pc as usize
+        self.state.pc as usize
     }
 }
 
@@ -141,10 +144,10 @@ mod tests {
         let e = parse::parse("MOV 10 ACC\nNOP").unwrap();
         let mut cpu = Cpu::new(e);
         assert_eq!(cpu.current_line(), 0);
-        assert_eq!(cpu.acc, 0);
+        assert_eq!(cpu.state.acc, 0);
         cpu.execute();
         assert_eq!(cpu.current_line(), 1);
-        assert_eq!(cpu.acc, 10);
+        assert_eq!(cpu.state.acc, 10);
     }
 
     #[test]
@@ -152,18 +155,18 @@ mod tests {
         let e = parse::parse("ADD 10\nADD -20\nSUB 10\nSUB -30").unwrap();
         let mut cpu = Cpu::new(e);
         assert_eq!(cpu.current_line(), 0);
-        assert_eq!(cpu.acc, 0);
+        assert_eq!(cpu.state.acc, 0);
         cpu.execute();
         assert_eq!(cpu.current_line(), 1);
-        assert_eq!(cpu.acc, 10);
+        assert_eq!(cpu.state.acc, 10);
         cpu.execute();
         assert_eq!(cpu.current_line(), 2);
-        assert_eq!(cpu.acc, -10);
+        assert_eq!(cpu.state.acc, -10);
         cpu.execute();
         assert_eq!(cpu.current_line(), 3);
-        assert_eq!(cpu.acc, -20);
+        assert_eq!(cpu.state.acc, -20);
         cpu.execute();
         assert_eq!(cpu.current_line(), 0);
-        assert_eq!(cpu.acc, 10);
+        assert_eq!(cpu.state.acc, 10);
     }
 }
