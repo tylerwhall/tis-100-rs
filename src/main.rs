60593e61338c2a06e4236d003ba5f3189375af58
Tyler Hall <tylerwhall@gmail.com>
2015-06-08 12:49:23 -0400

    Move modules into lib

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-pub mod instruction;
-pub mod parse;
-pub mod cpu;
+extern crate tis_100;
+use tis_100::parse;
 
+#[allow(dead_code)]
 fn main() {
     let program = parse::parse("TOP:\n NOP\nNOP\nJMP TOP\n").unwrap();
     println!("Program:\n {:?}", program);
