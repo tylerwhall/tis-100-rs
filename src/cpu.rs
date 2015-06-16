018ec416fe83d94e843fc4d3f433dcb01cef7508
Tyler Hall <tylerwhall@gmail.com>
2015-06-15 22:20:06 -0400

    cpu: ports overhaul
    - Use simpler CpuReadPorts
    - LAST port for reads
    - cycle-accurate write behavior
    - blocking read status

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,49 +1,50 @@
 use parse::Executable;
 use instruction;
 use instruction::{Instruction, Condition, Operand};
-use port;
-use port::{CpuWritePorts, GenericPort};
+use port::{CpuWritePorts, ReadPort};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ExecState {
+    EXEC,
+    READ(instruction::Port),
+    WRITE(instruction::Port),
+}
+
+impl Default for ExecState {
+    fn default() -> Self {
+        ExecState::EXEC
+    }
+}
 
 #[derive(Default)]
 pub struct CpuState {
-    pub acc: i32,
-    pub bak: i32,
-    pc: i32,
+    pub acc:        i32,
+    pub bak:        i32,
+    pc:             i32,
+    pending_write:  Option<(instruction::Port, i32)>,
+    exec_state:     ExecState,
 }
 
-pub struct CpuPorts {
-    up:     GenericPort,
-    down:   GenericPort,
-    left:   GenericPort,
-    right:  GenericPort,
-    last:   instruction::Port,
+pub struct CpuReadPorts<'a> {
+    up:     &'a ReadPort,
+    down:   &'a ReadPort,
+    left:   &'a ReadPort,
+    right:  &'a ReadPort,
 }
 
-impl CpuPorts {
-    fn new() -> Self {
-        CpuPorts {
-            up:     GenericPort::create(port::CpuPort::new()),
-            down:   GenericPort::create(port::CpuPort::new()),
-            left:   GenericPort::create(port::CpuPort::new()),
-            right:  GenericPort::create(port::CpuPort::new()),
-            last:   instruction::Port::Up,
-        }
-    }
-
+impl<'a> CpuReadPorts<'a> {
     /// Index ports structure by instruction port enum
-    fn match_port(&mut self, port: instruction::Port) -> &mut GenericPort {
-        match port {
-            instruction::Port::Up =>    &mut self.up,
-            instruction::Port::Down =>  &mut self.down,
-            instruction::Port::Left =>  &mut self.left,
-            instruction::Port::Right => &mut self.right,
-            _ => panic!("Unimplemented port")
+    fn get_port(&self, p: instruction::Port) -> &ReadPort {
+        match p {
+            instruction::Port::Up =>    self.up,
+            instruction::Port::Down =>  self.down,
+            instruction::Port::Left =>  self.left,
+            instruction::Port::Right => self.right,
+            _ => panic!("Invalid port")
         }
     }
 
-    fn read_port(&mut self, port: instruction::Port) -> Option<i32> {
-        let last = self.last.to_owned();
-
+    fn read_port(&mut self, port: instruction::Port, last: &mut instruction::Port) -> Option<i32> {
         match port {
             instruction::Port::Any => {
                 let mut ret = None;
@@ -51,47 +52,83 @@ impl CpuPorts {
                              instruction::Port::Down,
                              instruction::Port::Left,
                              instruction::Port::Right].iter() {
-                    ret = self.match_port(port.to_owned()).read();
+                    ret = self.get_port(*port).read();
                     if let Some(_) = ret {
-                        self.last = port.to_owned();
+                        *last = *port;
                         break;
                     }
                 }
                 ret
             },
-            instruction::Port::Last => self.match_port(last).read(),
+            instruction::Port::Last => self.get_port(*last).read(),
             _ => {
-                self.last = port.to_owned();
-                self.match_port(port).read()
+                self.get_port(port).read()
             }
         }
     }
 }
 
+struct CpuPorts<'a> {
+    outports:   &'a CpuWritePorts,
+    inports:    CpuReadPorts<'a>,
+    last:       instruction::Port,
+}
+
+impl<'a> CpuPorts<'a> {
+    fn read_port(&mut self, port: instruction::Port) -> Option<i32> {
+        self.inports.read_port(port, &mut self.last)
+    }
+
+    fn write_port(&mut self, port: instruction::Port, val: i32) {
+        self.outports.write_port(match port {
+            instruction::Port::Last => self.last,
+            _ => port,
+        }, val)
+    }
+
+    fn write_finished(&mut self, port: instruction::Port) -> bool {
+        let finished = self.outports.write_finished();
+        if finished && port == instruction::Port::Any {
+            self.last = self.outports.get_last();
+        }
+        finished
+    }
+}
+
 pub struct Cpu<'a> {
     state:      CpuState,
-    outports:   &'a CpuWritePorts,
-    inports:    CpuPorts,
+    ports:      CpuPorts<'a>,
     executable: Executable,
 }
 
-fn get_operand(state: &CpuState, ports: &mut CpuPorts, op: &Operand) -> Option<i32> {
+fn get_operand(state: &mut CpuState, ports: &mut CpuPorts, op: &Operand) -> Option<i32> {
     match op {
         &Operand::Lit(i) => Some(i),
         &Operand::ACC => Some(state.acc),
-        &Operand::Port(ref p) => ports.read_port(p.to_owned()),
+        &Operand::Port(p) => {
+            let val = ports.read_port(p);
+            if val == None {
+                state.exec_state = ExecState::READ(p)
+            } else {
+                state.exec_state = ExecState::EXEC
+            }
+            val
+        }
     }
 }
 
 impl<'a> Cpu<'a> {
-    pub fn new(executable: Executable, ports: &'a CpuWritePorts) -> Cpu<'a>{
-        let cpu = Cpu {
-            state: Default::default(),
-            outports: ports,
-            inports: CpuPorts::new(),
-            executable: executable,
+    pub fn new(executable: Executable, write_ports: &'a CpuWritePorts, read_ports: CpuReadPorts<'a>) -> Cpu<'a>{
+        let ports = CpuPorts {
+            outports:   write_ports,
+            inports:    read_ports,
+            last:       instruction::Port::Up,
         };
-        cpu
+        Cpu {
+            state: Default::default(),
+            ports: ports,
+            executable: executable,
+        }
     }
 
     pub fn execute(&mut self) -> bool {
@@ -99,14 +136,21 @@ impl<'a> Cpu<'a> {
             return false;
         }
 
+        // write_cycle() must be called between invocations of execute()
+        assert_eq!(self.state.pending_write, None);
+
+        if let ExecState::WRITE(_) = self.exec_state() {
+            return false;
+        }
+
         let advance_pc = match *self.executable.insn_at(self.pc()) {
             Instruction::NOP => true,
             Instruction::MOV { ref src, ref dst } => {
-                match get_operand(&self.state, &mut self.inports, src) {
+                match get_operand(&mut self.state, &mut self.ports, src) {
                     Some(i) => match dst {
                             &Operand::Lit(_) => panic!("Cannot store to a literal"),
                             &Operand::ACC => { self.state.acc = i; true },
-                            &Operand::Port(ref p) => self.outports.write_port(p.to_owned(), i),
+                            &Operand::Port(p) => { self.state.pending_write = Some((p, i)); false },
                     },
                     None => false
                 }
@@ -122,13 +166,13 @@ impl<'a> Cpu<'a> {
                 true
             },
             Instruction::ADD { ref addend } => {
-                match get_operand(&self.state, &mut self.inports, addend) {
+                match get_operand(&mut self.state, &mut self.ports, addend) {
                     Some(i) => { self.state.acc += i; true },
                     None => false
                 }
             },
             Instruction::SUB { ref subtrahend } => {
-                match get_operand(&self.state, &mut self.inports, subtrahend) {
+                match get_operand(&mut self.state, &mut self.ports, subtrahend) {
                     Some(i) => { self.state.acc -= i; true },
                     None => false
                 }
@@ -152,13 +196,48 @@ impl<'a> Cpu<'a> {
                 }
             },
             Instruction::JRO { ref dst } => {
-                match get_operand(&self.state, &mut self.inports, dst) {
+                match get_operand(&mut self.state, &mut self.ports, dst) {
                     Some(i) => { self.state.pc += i; true },
                     None => false
                 }
             },
         };
-        if advance_pc {
+        self.update_pc(advance_pc);
+        true
+    }
+
+    /// Processes writes from the last instruction executed
+    ///
+    /// This must be called after execute for each call to execute. The
+    /// write phase is separate from the execute phase to prevent reads and
+    /// writes between multiple CPUs from being dependent on the order in which
+    /// the CPUs are processed.
+    pub fn write_cycle(&mut self) {
+        if let Some((port, val)) = self.state.pending_write {
+            // This must succeed. Failure means trying to write while a write
+            // is already pending. CPU execution state should prevent that.
+            self.state.pending_write = None;
+            self.ports.write_port(port, val);
+            self.state.exec_state = ExecState::WRITE(port);
+        } else if let ExecState::WRITE(port) = self.state.exec_state {
+            // Check for write completion to advance pc
+            if self.ports.write_finished(port) {
+                self.state.exec_state = ExecState::EXEC;
+                self.update_pc(true);
+            }
+        }
+    }
+
+    pub fn current_line(&self) -> u32 {
+        self.executable.srcline_at(self.pc())
+    }
+
+    pub fn exec_state(&self) -> ExecState {
+        self.state.exec_state
+    }
+
+    fn update_pc(&mut self, advance: bool) {
+        if advance {
             self.state.pc += 1;
         }
         /* Handle wrapping at the end and via JRO */
@@ -166,11 +245,6 @@ impl<'a> Cpu<'a> {
         if self.state.pc < 0 {
             self.state.pc = self.executable.len() as i32 + self.state.pc;
         }
-        true
-    }
-
-    pub fn current_line(&self) -> u32 {
-        self.executable.srcline_at(self.pc())
     }
 
     fn pc(&self) -> usize {
@@ -180,16 +254,46 @@ impl<'a> Cpu<'a> {
 
 #[cfg(test)]
 mod tests {
-    use super::Cpu;
+    use super::{Cpu, CpuReadPorts, ExecState};
     use instruction;
-    use port::{CpuWritePorts, Port, ReadPort};
+    use port::{CpuWritePorts, CpuWritePortsReaders, Port, ReadPort};
     use parse;
+
+    #[derive(Default)]
+    struct DummyReadPort;
+
+    impl ReadPort for DummyReadPort {
+        fn read(&self) -> Option<i32> {
+            None
+        }
+    }
+
+    #[derive(Default)]
+    struct DummyReadPorts {
+        port:    DummyReadPort,
+    }
+
+    impl DummyReadPorts {
+        fn new() -> Self {
+            Default::default()
+        }
+
+        fn cpuports(&self) -> CpuReadPorts {
+            CpuReadPorts {
+                up      : &self.port as &ReadPort,
+                down    : &self.port as &ReadPort,
+                left    : &self.port as &ReadPort,
+                right   : &self.port as &ReadPort,
+            }
+        }
+    }
 
     #[test]
     fn test_cpu_wrapping() {
         let e = parse::parse("TOP: NOP\nNOP").unwrap();
-        let ports: CpuWritePorts = Default::default();
-        let mut cpu = Cpu::new(e, &ports);
+        let ports = CpuWritePorts::new();
+        let rports = DummyReadPorts::new();
+        let mut cpu = Cpu::new(e, &ports, rports.cpuports());
         assert_eq!(cpu.current_line(), 0);
         cpu.execute();
         assert_eq!(cpu.current_line(), 1);
@@ -203,8 +307,9 @@ mod tests {
     #[test]
     fn test_mov() {
         let e = parse::parse("MOV 10 ACC\nNOP").unwrap();
-        let ports: CpuWritePorts = Default::default();
-        let mut cpu = Cpu::new(e, &ports);
+        let ports = CpuWritePorts::new();
+        let rports = DummyReadPorts::new();
+        let mut cpu = Cpu::new(e, &ports, rports.cpuports());
         assert_eq!(cpu.current_line(), 0);
         assert_eq!(cpu.state.acc, 0);
         cpu.execute();
@@ -215,8 +320,9 @@ mod tests {
     #[test]
     fn test_add_sub() {
         let e = parse::parse("ADD 10\nADD -20\nSUB 10\nSUB -30").unwrap();
-        let ports: CpuWritePorts = Default::default();
-        let mut cpu = Cpu::new(e, &ports);
+        let ports = CpuWritePorts::new();
+        let rports = DummyReadPorts::new();
+        let mut cpu = Cpu::new(e, &ports, rports.cpuports());
         assert_eq!(cpu.current_line(), 0);
         assert_eq!(cpu.state.acc, 0);
         cpu.execute();
@@ -234,21 +340,167 @@ mod tests {
     }
 
     #[test]
-    fn test_ports() {
-        let e = parse::parse("MOV 10 DOWN").unwrap();
-        let ports: CpuWritePorts = Default::default();
-        let mut cpu = Cpu::new(e, &ports);
+    fn test_port_write() {
+        let e = parse::parse("MOV 10 DOWN\nNOP").unwrap();
+        let ports = CpuWritePorts::new();
+        let rports = DummyReadPorts::new();
+        let mut cpu = Cpu::new(e, &ports, rports.cpuports());
+
+        // First interation. Make sure writes appear immediately after the first write_cycle()
         cpu.execute();
-        assert_eq!(cpu.outports.get_read_port(instruction::Port::Down).read().unwrap(), 10);
+        assert_eq!(cpu.ports.outports.get_read_port(instruction::Port::Down).read(), None);
+        cpu.write_cycle();
+        assert_eq!(cpu.current_line(), 0);
+        assert_eq!(cpu.exec_state(), ExecState::WRITE(instruction::Port::Down));
+        assert_eq!(cpu.ports.outports.get_read_port(instruction::Port::Down).read().unwrap(), 10);
+        assert_eq!(cpu.exec_state(), ExecState::WRITE(instruction::Port::Down));
+
+        cpu.execute();
+        assert_eq!(cpu.current_line(), 0);
+        assert_eq!(cpu.exec_state(), ExecState::WRITE(instruction::Port::Down));
+        cpu.write_cycle();
+        assert_eq!(cpu.exec_state(), ExecState::EXEC);
+        assert_eq!(cpu.current_line(), 1);
+
+        // NOP
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(cpu.current_line(), 0);
+
+        // Second interation. Make sure writes block
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(cpu.current_line(), 0);
+        assert_eq!(cpu.exec_state(), ExecState::WRITE(instruction::Port::Down));
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(cpu.current_line(), 0);
+        assert_eq!(cpu.exec_state(), ExecState::WRITE(instruction::Port::Down));
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(cpu.ports.outports.get_read_port(instruction::Port::Down).read().unwrap(), 10);
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(cpu.exec_state(), ExecState::EXEC);
+        assert_eq!(cpu.current_line(), 1);
+    }
+
+    /// Ensures write ANY can be read from any output, but only one output
+    #[test]
+    fn test_write_any_last() {
+        let e = parse::parse("MOV 10 ANY\n
+                              MOV 20 ANY\n
+                              MOV 30 ANY\n
+                              MOV 40 ANY\n
+                              MOV 50 LAST").unwrap();
+        let ports = CpuWritePorts::new();
+        let rports = DummyReadPorts::new();
+        let mut cpu = Cpu::new(e, &ports, rports.cpuports());
+
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(ports.get_read_port(instruction::Port::Up).read().unwrap(), 10);
+        assert_eq!(ports.get_read_port(instruction::Port::Up).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Down).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Left).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Right).read(), None);
+        cpu.execute();
+        cpu.write_cycle();
+
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(ports.get_read_port(instruction::Port::Down).read().unwrap(), 20);
+        assert_eq!(ports.get_read_port(instruction::Port::Up).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Down).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Left).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Right).read(), None);
+        cpu.execute();
+        cpu.write_cycle();
+
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(ports.get_read_port(instruction::Port::Left).read().unwrap(), 30);
+        assert_eq!(ports.get_read_port(instruction::Port::Up).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Down).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Left).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Right).read(), None);
+        cpu.execute();
+        cpu.write_cycle();
+
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(ports.get_read_port(instruction::Port::Right).read().unwrap(), 40);
+        assert_eq!(ports.get_read_port(instruction::Port::Up).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Down).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Left).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Right).read(), None);
+        cpu.execute();
+        cpu.write_cycle();
+
+        // Last
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(ports.get_read_port(instruction::Port::Up).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Down).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Left).read(), None);
+        assert_eq!(ports.get_read_port(instruction::Port::Right).read().unwrap(), 50);
+        assert_eq!(ports.get_read_port(instruction::Port::Right).read(), None);
     }
 
     #[test]
     fn port_borrow() {
         let e = parse::parse("MOV 10 DOWN").unwrap();
         let ports = CpuWritePorts::new();
+        let rports = DummyReadPorts::new();
         let down = ports.get_read_port(instruction::Port::Down);
-        let mut cpu = Cpu::new(e, &ports);
+        let mut cpu = Cpu::new(e, &ports, rports.cpuports());
         cpu.execute();
+        cpu.write_cycle();
         assert_eq!(down.read().unwrap(), 10);
+    }
+
+    #[test]
+    fn blocking_read() {
+        let e = parse::parse("MOV UP DOWN\nMOV DOWN ACC").unwrap();
+        let ports = CpuWritePorts::new();
+        let inports = CpuWritePorts::new();
+        let rports = CpuWritePortsReaders::from(&inports);
+        let rports = CpuReadPorts {
+            up:     &rports.up,
+            down:   &rports.down,
+            left:   &rports.left,
+            right:  &rports.right,
+        };
+
+        let mut cpu = Cpu::new(e, &ports, rports);
+
+        // READ -> WRITE -> EXEC state
+        assert_eq!(cpu.exec_state(), ExecState::EXEC);
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(cpu.exec_state(), ExecState::READ(instruction::Port::Up));
+        inports.write_port(instruction::Port::Up, 10);
+        assert_eq!(ports.get_read_port(instruction::Port::Down).read(), None);
+
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(ports.get_read_port(instruction::Port::Down).read().unwrap(), 10);
+        assert_eq!(cpu.exec_state(), ExecState::WRITE(instruction::Port::Down));
+
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(ports.get_read_port(instruction::Port::Down).read(), None);
+        assert_eq!(cpu.exec_state(), ExecState::EXEC);
+
+        // READ -> EXEC state
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(cpu.exec_state(), ExecState::READ(instruction::Port::Down));
+        inports.write_port(instruction::Port::Down, 20);
+
+        cpu.execute();
+        cpu.write_cycle();
+        assert_eq!(cpu.exec_state(), ExecState::EXEC);
+        assert_eq!(cpu.state.acc, 20);
     }
 }
--- src/port.rs
+++ src/port.rs
@@ -1,5 +1,3 @@
-use std::rc::Rc;
-use std::cell::RefCell;
 use std::cell::Cell;
 use instruction;
 
@@ -12,17 +10,23 @@ pub trait ReadPort {
     fn read(&self) -> Option<i32>;
 }
 
-#[derive(Default)]
 pub struct CpuWritePorts {
     up:     Cell<Option<i32>>,
     down:   Cell<Option<i32>>,
     left:   Cell<Option<i32>>,
     right:  Cell<Option<i32>>,
+    last:   Cell<instruction::Port>,
 }
 
 impl CpuWritePorts {
     pub fn new() -> Self {
-        Default::default()
+        CpuWritePorts {
+            up:     Default::default(),
+            down:   Default::default(),
+            left:   Default::default(),
+            right:  Default::default(),
+            last:   Cell::new(instruction::Port::Up),
+        }
     }
 
     fn get_port(&self, p: instruction::Port) -> &Cell<Option<i32>> {
@@ -52,12 +56,13 @@ impl CpuWritePorts {
              * This works for the write ANY case, but also works for a write to a
              * specific port because only one pending write is allowed at once */
             self.set_all(None);
+            self.last.set(port);
         }
         ret
     }
 
-    /// Returns true if a reader has consumed any port
-    fn write_any_finished(&self) -> bool {
+    /// Returns true if no write is pending
+    pub fn write_finished(&self) -> bool {
         match self.up.get()
             .or(self.down.get())
             .or(self.left.get())
@@ -67,6 +72,10 @@ impl CpuWritePorts {
         }
     }
 
+    pub fn get_last(&self) -> instruction::Port {
+        self.last.get()
+    }
+
     pub fn get_read_port(&self, p: instruction::Port) -> CpuWritePortsReader {
         CpuWritePortsReader::new(self, p)
     }
@@ -74,13 +83,12 @@ impl CpuWritePorts {
     /// Store from the CPU into the port
     ///
     /// Returns false if the port is full
-    pub fn write_port(&self, p: instruction::Port, val: i32) -> bool {
-        let port = self.get_port(p);
-        if port.get() == None {
-            port.set(Some(val));
-            true
-        } else {
-            false
+    /// Accepts a direction or any (not last)
+    pub fn write_port(&self, p: instruction::Port, val: i32) {
+        assert!(self.write_finished());
+        match p {
+            instruction::Port::Any => self.set_all(Some(val)),
+            _ => self.get_port(p).set(Some(val))
         }
     }
 }
@@ -109,82 +117,32 @@ impl<'a> ReadPort for CpuWritePortsReader<'a> {
     }
 }
 
-/// The producer, or the output side of a CPU.
-#[derive(Default)]
-pub struct CpuPort {
-    val: Option<i32>
+pub struct CpuWritePortsReaders<'a> {
+    pub up:     CpuWritePortsReader<'a>,
+    pub down:   CpuWritePortsReader<'a>,
+    pub left:   CpuWritePortsReader<'a>,
+    pub right:  CpuWritePortsReader<'a>,
 }
 
-impl Port for CpuPort {
-    /// Read from the CPU's output
-    ///
-    /// Returns None if the port is empty
-    fn read(&mut self) -> Option<i32> {
-        // May return None if val is already None
-        let tmp = self.val;
-        self.val = None;
-        tmp
-    }
-
-    /// Store from the CPU into the port
-    ///
-    /// Returns false if the port is full
-    fn write(&mut self, val: i32) -> bool {
-        if self.val == None {
-            self.val = Some(val);
-            true
-        } else {
-            false
+impl<'a> From<&'a CpuWritePorts> for CpuWritePortsReaders<'a> {
+    fn from(ports: &'a CpuWritePorts) -> Self {
+        CpuWritePortsReaders {
+            up:     ports.get_read_port(instruction::Port::Up),
+            down:   ports.get_read_port(instruction::Port::Down),
+            left:   ports.get_read_port(instruction::Port::Left),
+            right:  ports.get_read_port(instruction::Port::Right),
         }
     }
 }
 
-impl CpuPort {
-    pub fn new() -> Self {
-        Default::default()
-    }
-}
-
-pub struct GenericPort(Rc<RefCell<Box<Port>>>);
-
-impl GenericPort {
-    pub fn create<T: Port + 'static>(x: T) -> GenericPort {
-        GenericPort(Rc::new(RefCell::new(Box::new(x) as Box<Port>)))
-    }
-
-    pub fn read(&self) -> Option<i32> {
-        self.0.borrow_mut().read()
-    }
-
-    pub fn write(&self, val: i32) -> bool {
-        self.0.borrow_mut().write(val)
-    }
-
-    pub fn clone(&self) -> GenericPort {
-        GenericPort(self.0.clone())
-    }
-}
-
-
 #[cfg(test)]
 mod tests {
-    use super::{CpuPort, CpuWritePorts, Port, ReadPort};
+    use super::{CpuWritePorts, Port, ReadPort};
     use instruction;
 
     #[test]
-    fn test_cpu_port() {
-        let mut port: CpuPort = Default::default();
-
-        assert_eq!(port.read(), None);
-        assert_eq!(port.write(1), true);
-        assert_eq!(port.write(1), false);
-        assert_eq!(port.read(), Some(1));
-        assert_eq!(port.read(), None);
-    }
-
-    #[test]
     fn test_cpu_write_ports() {
-         let ports: CpuWritePorts = Default::default();
+         let ports = CpuWritePorts::new();
 
          let port = ports.get_read_port(instruction::Port::Up);
          ports.write_port(instruction::Port::Up, 1);
